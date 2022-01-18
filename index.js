d6fe447e5ca752f82e74312f88cb6ef69c82d6c7
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-01-17 22:08:02 -1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+var mySupervillainName = supervillains.random();
+
+console.log(mySupervillainName);
